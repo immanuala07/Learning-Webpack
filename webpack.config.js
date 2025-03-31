fdfe38dc947d9193eb0cbeb83fc629cbcc8e1fa7
Immanual <immanualatrinity@gmail.com>
2025-03-31 10:58:16 +0530

    publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        // publicPath in Webpack defines the base URL for all assets in your project. It tells Webpack where to serve or load assets from.
+        // It's important when deploying to different environments (local, CDN, subfolder).
+        publicPath: 'dist/'
     },
     mode: 'none',
     module:{
